bcec5837725bf8f450857adde113b39d51b73a3e
Mark Mankarious <mz.mankarious@gmail.com>
2019-10-19 10:17:48 +0100

    Updates deployment config

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -2,10 +2,8 @@ source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/droneX/api
 unzip api.zip
-npm run start
 
 cd /home/ec2-user/droneX/frontend
 unzip frontend.zip
-npx serve -l 3000 .
 
 sudo service nginx restart
