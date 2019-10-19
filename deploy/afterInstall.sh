7465646caf99aa204ce41fcd58b6e03c9477a0cd
Mark Mankarious <mz.mankarious@gmail.com>
2019-10-19 09:46:52 +0100

    Updates deployment config

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 source ~/.bashrc
 
-PATH=/home/ec2-user/.nvm/versions/node/v10.0.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin
-export PATH
-
-echo $PATH
-echo $(node -v)
-
 cd /home/ec2-user/droneX/api
-extract-zip api.zip .
-npm run start
+sudo -H -u ec2-user bash -c 'extract-zip api.zip . && npm run start'
 
 cd /home/ec2-user/droneX/frontend
-extract-zip frontend.zip .
-npx serve -l 3000 .
+sudo -H -u ec2-user bash -c 'extract-zip frontend.zip . && npx serve -l 3000 .'
 
 sudo su
 sudo service nginx restart
