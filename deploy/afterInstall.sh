426dd3c5a7bedfcf915dd80babbed9cf76810b16
Mark Mankarious <mz.mankarious@gmail.com>
2019-10-19 09:35:50 +0100

    Updates deployment config

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -1,4 +1,7 @@
-source ~/.bash_profile
+source ~/.bashrc
+
+PATH=/home/ec2-user/.nvm/versions/node/v10.0.0/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin
+export PATH
 
 echo $PATH
 echo $(node -v)
