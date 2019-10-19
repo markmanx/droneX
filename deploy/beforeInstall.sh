3f19636179ede8e2c818a7df0289cd47d627c9da
Mark Mankarious <mz.mankarious@gmail.com>
2019-10-19 08:02:55 +0100

    Updates deployment config

--- deploy/beforeInstall.sh
+++ deploy/beforeInstall.sh
@@ -1,3 +1,3 @@
-rm -rf /home/ec2-user/droneX/api
-rm -rf /home/ec2-user/droneX/frontend
-rm /etc/nginx/sites-enabled/droneX.nginx.conf
+rm -rf /home/ec2-user/droneX/api || true
+rm -rf /home/ec2-user/droneX/frontend || true
+rm -f /etc/nginx/sites-enabled/droneX.nginx.conf || true
