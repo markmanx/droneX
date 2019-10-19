8a9adcd5ae7b4e815e51c9dcbcc81a461db03b11
Mark Mankarious <mz.mankarious@gmail.com>
2019-10-19 08:50:10 +0100

    Updates deployment config

--- deploy/afterInstall.sh
+++ deploy/afterInstall.sh
@@ -1,4 +1,5 @@
 source ~/.bash_profile
+export NVM_DIR="/home/ec2-user/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
 cd /home/ec2-user/droneX/api
 extract-zip api.zip .
--- deploy/beforeInstall.sh
+++ deploy/beforeInstall.sh
@@ -1,3 +1,5 @@
+source ~/.bash_profile
+
 rm -rf /home/ec2-user/droneX/api || true
 rm -rf /home/ec2-user/droneX/frontend || true
 rm -f /etc/nginx/sites-enabled/droneX.nginx.conf || true
